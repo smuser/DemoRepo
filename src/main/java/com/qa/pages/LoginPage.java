b2440de6bddfc0ef124a79e01173a03aa345f854
Deepak Chawda <sheenm1530@gmail.com>
2023-05-06 09:11:57 -0700

    New commit to git code

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class LoginPage {
+	public void testLogin()
+	{
+		System.out.println("Basic test");
+	}
+}
---
+++ src/main/java/com/qa/util/UtilClass.java
@@ -0,0 +1,10 @@
+package com.qa.util;
+
+public class UtilClass {
+	
+	
+	public void utility()
+	{
+		System.out.println("Basic test utility");
+	}
+}
