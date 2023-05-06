d8d1f3b3ae58412d453f00b450d1e4b58c67b9ed
Deepak Chawda <sheenm1530@gmail.com>
2023-05-06 10:02:29 -0700

    Added new search page

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,5 +1,18 @@
 package com.qa.tests;
 
+import org.testng.annotations.Test;
+
 public class LoginTest {
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Test the login page");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Testing added to check in code");
+	}
 
 }
---
+++ src/test/java/com/qa/tests/SearchPage.java
@@ -0,0 +1,8 @@
+package com.qa.tests;
+
+public class SearchPage {
+	public void testSearch()
+	{
+		System.out.println("Basic test");
+	}
+}
